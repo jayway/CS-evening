0fc6026f117159cf40b1bbffb8a53c5e13e604a8
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-05-06 15:57:50 +0200

    added doc.

--- TSP/src/TSPValidator.java
+++ TSP/src/TSPValidator.java
@@ -4,17 +4,29 @@ import java.util.List;
 
 public class TSPValidator {
 
+    /**
+     * Calculate the length of a path.
+     *
+     * @param arcs matrix containing the arcs of the graph
+     * @param path the path to get a length for
+     * @return the length
+     */
     public static long getPathLength(int[][] arcs, int[] path) {
         long l = 0;
-
         for (int i = 1; i < path.length; i++) {
             l += arcs[path[i - 1]][path[i]];
         }
-
         return l;
     }
 
-    public static boolean isPathValid(int[][] arcs, int[] path) {
+    /**
+     * Check that the path contains every index
+     * exactly once.
+     *
+     * @param path the path to check
+     * @return the outcome of the check
+     */
+    public static boolean isPathValid(int[] path) {
         List<Integer> list = new ArrayList<Integer>();
         for (int i = 0; i < path.length; i++) {
             list.add(path[i]);
