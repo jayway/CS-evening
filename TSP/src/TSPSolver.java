64bf59544b82099ee4e9a92b3543c557fb2bfa13
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-05-06 15:57:50 +0200

    added doc.

--- TSP/src/TSPSolver.java
+++ TSP/src/TSPSolver.java
@@ -4,13 +4,15 @@ public class TSPSolver {
         int[][] arcs = Data.CITY_DISTANCES;
         int[] path = new int[arcs[0].length];
 
+        // Create a valid but very naive solution by
+        // setting 0..n to be our path.
         for (int i = 0; i < path.length; i++) {
             path[i] = i;
         }
 
         long length = TSPValidator.getPathLength(arcs, path);
 
-        boolean valid = TSPValidator.isPathValid(arcs, path);
+        boolean valid = TSPValidator.isPathValid(path);
 
         System.out.println("This TSP path is " + length + " km long and is "
                 + (valid ? "valid" : "invalid"));
