6dd1d837f87f11c62b2e1faaa7be95a901721cee
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-05-06 15:51:21 +0200

    added a validator.

--- TSP/src/TSPSolver.java
+++ TSP/src/TSPSolver.java
@@ -1,27 +1,20 @@
-
 public class TSPSolver {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[][] arcs = Data.CITY_DISTANCES;
         int[] path = new int[arcs[0].length];
 
-        for(int i=0;i<path.length;i++){
-            path[i]=i;
+        for (int i = 0; i < path.length; i++) {
+            path[i] = i;
         }
 
-        long length = getPathLength(arcs, path);
+        long length = TSPValidator.getPathLength(arcs, path);
 
-        System.out.println("This TSP path is " + length + " km long.");
+        boolean valid = TSPValidator.isPathValid(arcs, path);
+
+        System.out.println("This TSP path is " + length + " km long and is "
+                + (valid ? "valid" : "invalid"));
 
     }
 
-    private static long getPathLength(int[][] arcs, int[] path) {
-        long l=0;
-
-        for(int i=1;i<path.length;i++){
-            l += arcs[path[i-1]][path[i]];
-        }
-
-        return l;
-    }
 }
