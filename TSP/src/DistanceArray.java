9288b1015c72806174ba2e5ca0263a60de52dbe2
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-05-15 11:55:26 +0200

    minor update.

--- TSP/src/DistanceArray.java
+++ TSP/src/DistanceArray.java
@@ -4,16 +4,20 @@ public class DistanceArray {
         int[][] array = new int[data.length / 2][data.length / 2];
 
         for (int i = 0; i < data.length / 2; i++) {
-            int x1 = data[i * 2];
-            int y1 = data[i * 2 + 1];
             for (int j = 0; j < data.length / 2; j++) {
-                int x2 = data[j * 2];
-                int y2 = data[j * 2 + 1];
-                array[i][j] = (int) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+                array[i][j] = getDistance(data, i, j);
             }
         }
 
         return array;
+    }
+
+    public static int getDistance(int[] data, int p1, int p2) {
+        int x1 = data[p1 * 2];
+        int y1 = data[p1 * 2 + 1];
+        int x2 = data[p2 * 2];
+        int y2 = data[p2 * 2 + 1];
+        return (int) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 
 }
