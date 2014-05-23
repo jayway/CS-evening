29f895183ed1b4436c419736b1bb3e47363967e2
Johan <jnordenswan@gmail.com>
2014-05-23 10:37:11 +0200

    fresh sample data

--- nodegen/nodegen.py
+++ nodegen/nodegen.py
@@ -1,5 +1,6 @@
 from random import sample
 import copy
+from sys import argv
 from sys import stdout
 write = stdout.write
 
@@ -22,3 +23,10 @@ class TSP_Map(object):
         write(str(self.tsp_map[0][0]) + ',' + str(self.tsp_map[0][1]))
         for e in self.tsp_map[1:]:
             write(',' + str(e[0]) + ',' + str(e[1]))
+
+if __name__ == '__main__':
+    width = int(argv[1])
+    height = int(argv[2])
+    no_of_nodes = int(argv[3])
+    tsp_map = TSP_Map(width, height, no_of_nodes)
+    tsp_map.write_as_csv()
