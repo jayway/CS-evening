2b4a33f6bf94357b9c1ced5923d2463697409705
Carl-Emil Kjellstrand <erbsman@gmail.com>
2014-06-16 20:43:32 +0200

    started a node insert thingy.

--- Carl-Emil_TSP/src/TSPSolver.java
+++ Carl-Emil_TSP/src/TSPSolver.java
@@ -1,25 +1,78 @@
 public class TSPSolver {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // int[] data = RandomData.getRandomData(size, w, h, 0);
-        int[] data = TSPTools.readGraphFromCVSFile("../nodegen/10_locations.csv");
+		// int[] data = RandomData.getRandomData(size, w, h, 0);
+		int[] data = TSPTools.readGraphFromCVSFile("../nodegen/10_locations.csv");
 
-        int size = data.length / 2;
+		int size = data.length / 2;
 
-        System.out.println("size= " + size);
+		System.out.println("size= " + size);
 
-        int[][] arcs = Arcs.getArray(data, size, size);
+		int[][] arcs = Arcs.getArray(data, size, size);
 
-        TSPTools.printArcs(size, arcs);
+		TSPTools.printArcs(size, arcs);
 
-        int[] path = new int[size];
+		int[] path = new int[size];
 
-        for (int i = 0; i < size; i++) {
-            path[i] = i;
-        }
+		for (int i = 0; i < size; i++) {
+			path[i] = i;
+		}
+		boolean improved = true;
+		long best = Integer.MAX_VALUE;
+		while (improved) {
+			long last = tryRemoveAndInsert(arcs, path);
+			improved = last < best;
+		}
+		// removeXarcs(path, arcs);
 
-        TSPTools.checkPath(arcs, path);
-    }
+		TSPTools.checkPath(arcs, path);
+	}
+
+	private static long tryRemoveAndInsert(int[][] arcs, int[] path) {
+
+		// for (int n = 1; n < path.length - 1; n++) {
+		// for (int i = 1; i < path.length - 1; i++) {
+		// System.out.println("n: " + n + ", i: " + i);
+		int n = 1, i = 5;
+		int d1 = arcs[n][n - 1] + arcs[n][n + 1] + arcs[i][i + 1];
+		int d2 = arcs[n][i] + arcs[n][i + 1] + arcs[n - 1][n + 1];
+		if (d1 > d2) {
+			if (n < i) {
+				int v = path[n];
+				System.out.println("n " + n + ", i:" + i + ", d1: " + d1 + ", d2: " + d2);
+				for (int j = n; j < i; j++) {
+					path[j] = path[j + 1];
+					TSPTools.printPath(path);
+				}
+				path[i] = v;
+				TSPTools.printPath(path);
+				TSPTools.checkPath(arcs, path);
+				System.out.println("-----------");
+			} else {
+
+			}
+			// break;
+		}
+		// }
+		// }
+		return TSPTools.getPathLength(arcs, path);
+	}
+
+	private static void removeXarcs(int[] path, int[][] arcs) {
+		for (int i = 0; i < path.length; i++) {
+			int a = path[i];
+			int b = path[i + 1];
+			for (int j = i + 2; j < path.length; j++) {
+				int c = path[j];
+				int d = path[j + 1];
+				int a1 = arcs[a][b] + arcs[c][d];
+				int a2 = arcs[a][d] + arcs[b][c];
+				if (a1 > a2) {
+					TSPTools.reversSubSectionOfArray(path, i, j + 1);
+				}
+			}
+		}
+	}
 
 }
--- Carl-Emil_TSP/src/TSPTools.java
+++ Carl-Emil_TSP/src/TSPTools.java
@@ -7,110 +7,111 @@ import java.util.List;
 
 public class TSPTools {
 
-    /**
-     * 
-     * @param arcs
-     * @param path
-     */
-    public static void checkPath(int[][] arcs, int[] path) {
-        long length = TSPTools.getPathLength(arcs, path);
-        boolean valid = TSPTools.isPathValid(path);
-        System.out.println("This TSP path is " + length + " km long and is "
-                + (valid ? "valid" : "invalid") + ".");
-        TSPTools.printPath(path);
-    }
+	/**
+	 * 
+	 * @param arcs
+	 * @param path
+	 */
+	public static void checkPath(int[][] arcs, int[] path) {
+		long length = TSPTools.getPathLength(arcs, path);
+		boolean valid = TSPTools.isPathValid(path);
+		System.out.println("This TSP path is " + length + " km long and is " + (valid ? "valid" : "invalid") + ".");
+		TSPTools.printPath(path);
+	}
 
-    /**
-     * Calculate the length of a path.
-     * 
-     * @param arcs matrix containing the arcs of the graph
-     * @param path the path to get a length for
-     * @return the length
-     */
-    public static long getPathLength(int[][] arcs, int[] path) {
-        long l = 0;
-        for (int i = 1; i < path.length; i++) {
-            System.out.println(i + "  " + path[i - 1] + "  " + path[i]);
-            l += arcs[path[i - 1]][path[i]];
-        }
-        l += arcs[path.length - 1][0];
-        return l;
-    }
+	/**
+	 * Calculate the length of a path.
+	 * 
+	 * @param arcs
+	 *            matrix containing the arcs of the graph
+	 * @param path
+	 *            the path to get a length for
+	 * @return the length
+	 */
+	public static long getPathLength(int[][] arcs, int[] path) {
+		long l = 0;
+		for (int i = 1; i < path.length; i++) {
+			l += arcs[path[i - 1]][path[i]];
+		}
+		l += arcs[path.length - 1][0];
+		return l;
+	}
 
-    /**
-     * Check that the path contains every index exactly once.
-     * 
-     * @param path the path to check
-     * @return the outcome of the check
-     */
-    public static boolean isPathValid(int[] path) {
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < path.length; i++) {
-            list.add(path[i]);
-        }
-        Collections.sort(list);
-        for (int i = 0; i < path.length; i++) {
-            if (list.get(i) != i) {
-                return false;
-            }
-        }
-        return true;
-    }
+	/**
+	 * Check that the path contains every index exactly once.
+	 * 
+	 * @param path
+	 *            the path to check
+	 * @return the outcome of the check
+	 */
+	public static boolean isPathValid(int[] path) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < path.length; i++) {
+			list.add(path[i]);
+		}
+		Collections.sort(list);
+		for (int i = 0; i < path.length; i++) {
+			if (list.get(i) != i) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    /**
-     * 
-     * @param array
-     * @param reverseStart
-     * @param reverseEnd
-     */
-    public void reversSubSectionOfArray(int[] array, int reverseStart, int reverseEnd) {
+	/**
+	 * 
+	 * @param array
+	 * @param reverseStart
+	 * @param reverseEnd
+	 */
+	public static void reversSubSectionOfArray(int[] array, int reverseStart, int reverseEnd) {
 
-    }
+	}
 
-    /**
-     * 
-     * @param path
-     * @return
-     * @throws IOException
-     */
-    public static int[] readGraphFromCVSFile(String file) {
-        BufferedReader br = null;
-        String line = "";
-        try {
-            br = new BufferedReader(new FileReader(file));
-            line = br.readLine();
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String[] split = line.split(",");
-        int[] results = new int[split.length];
+	/**
+	 * 
+	 * @param path
+	 * @return
+	 * @throws IOException
+	 */
+	public static int[] readGraphFromCVSFile(String file) {
+		BufferedReader br = null;
+		String line = "";
+		try {
+			br = new BufferedReader(new FileReader(file));
+			line = br.readLine();
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String[] split = line.split(",");
+		int[] results = new int[split.length];
 
-        for (int i = 0; i < split.length; i++) {
-            try {
-                results[i] = Integer.parseInt(split[i]);
-            } catch (NumberFormatException nfe) {
-                nfe.printStackTrace();
-            }
-        }
-        return results;
-    }
+		for (int i = 0; i < split.length; i++) {
+			try {
+				results[i] = Integer.parseInt(split[i]);
+			} catch (NumberFormatException nfe) {
+				nfe.printStackTrace();
+			}
+		}
+		return results;
+	}
 
-    public static void printPath(int[] path) {
-        System.out.print("Path: ");
-        for (int i = 0; i < path.length; i++) {
-            System.out.print(path[i] + ", ");
-        }
-        System.out.println();
-    }
+	public static void printPath(int[] path) {
+		System.out.print("Path: ");
+		for (int i = 0; i < path.length; i++) {
+			System.out.print(path[i] + ", ");
+		}
+		System.out.println();
+	}
 
-    public static void printArcs(int size, int[][] distanceArray) {
-        System.out.println(" - Distance array - ");
-        for (int i = 0; i < size / 2; i++) {
-            for (int j = 0; j < size / 2; j++) {
-                System.out.print(" " + distanceArray[i][j]);
-            }
-            System.out.println();
-        }
-    }
+	public static void printArcs(int size, int[][] distanceArray) {
+		System.out.println(" - Distance array - ");
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				System.out.print(" " + distanceArray[i][j]);
+			}
+			System.out.println();
+		}
+	}
 }
