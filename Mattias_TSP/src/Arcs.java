b379d1fe2d4f538ec8a2f560e5827185fd850530
Mattias Rosberg <mattias.rosberg@jayway.com>
2014-06-16 21:15:11 +0200

    Initital version

---
+++ Mattias_TSP/src/Arcs.java
@@ -0,0 +1,23 @@
+public class Arcs {
+
+    public static int[][] getArray(int[] data, int w, int h) {
+        int[][] array = new int[data.length / 2][data.length / 2];
+
+        for (int i = 0; i < data.length / 2; i++) {
+            for (int j = 0; j < data.length / 2; j++) {
+                array[i][j] = getDistance(data, i, j);
+            }
+        }
+
+        return array;
+    }
+
+    public static int getDistance(int[] data, int p1, int p2) {
+        int x1 = data[p1 * 2];
+        int y1 = data[p1 * 2 + 1];
+        int x2 = data[p2 * 2];
+        int y2 = data[p2 * 2 + 1];
+        return (int) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    }
+
+}
---
+++ Mattias_TSP/src/TSPTools.java
@@ -0,0 +1,163 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class TSPTools {
+
+    /**
+     * 
+     * @param arcs
+     * @param path
+     */
+    public static void checkPath(int[][] arcs, int[] path) {
+        long length = TSPTools.getPathLength(arcs, path);
+        boolean valid = TSPTools.isPathValid(path);
+        System.out.println("This TSP path is " + length + " km long and is "
+                + (valid ? "valid" : "invalid") + ".");
+        TSPTools.printPath(path);
+    }
+
+    /**
+     * Calculate the length of a path.
+     * 
+     * @param arcs matrix containing the arcs of the graph
+     * @param path the path to get a length for
+     * @return the length
+     */
+    public static long getPathLength(int[][] arcs, int[] path) {
+        long l = 0;
+        for (int i = 1; i < path.length; i++) {
+//            System.out.println(i + "  " + path[i - 1] + "  " + path[i]);
+            l += arcs[path[i - 1]][path[i]];
+        }
+        l += arcs[path[path.length - 1]][path[0]];
+        return l;
+    }
+
+    /**
+     * Check that the path contains every index exactly once.
+     * 
+     * @param path the path to check
+     * @return the outcome of the check
+     */
+    public static boolean isPathValid(int[] path) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < path.length; i++) {
+            list.add(path[i]);
+        }
+        Collections.sort(list);
+        for (int i = 0; i < path.length; i++) {
+            if (list.get(i) != i) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 
+     * @param array
+     * @param reverseStart
+     * @param reverseEnd
+     */
+    public void reverseSubSectionOfArray(int[] array, int reverseStart, int reverseEnd) {
+        while(reverseStart < reverseEnd){
+            int temp = array[reverseStart];
+            array[reverseStart] = array[reverseEnd];
+            array[reverseEnd] = temp;
+            reverseStart++;
+            reverseEnd--;
+        }
+    }
+
+    public void removeTangleFromPath(int[] initialPath){
+        for (int i = 0; i < initialPath.length; i++) {
+            for (int j = i+1; j < initialPath.length; j++) {
+
+
+            }
+        }
+    }
+
+    public static int[] getShortestPathFromOneNode(int[][] arcs, int size, int startNode){
+        ArrayList<Integer> tempPath = new ArrayList<Integer>();
+        HashSet<Integer> possibleNodes = new HashSet<Integer>();
+        possibleNodes.addAll(Arrays.asList(new Integer[]{0,1,2,3,4,5,6,7,8,9}));
+        tempPath.add(startNode);
+        possibleNodes.remove(startNode);
+
+        while(possibleNodes.size() > 0){
+            int nextNode = getClosestNeighborForNode(arcs, tempPath.get(tempPath.size()-1), possibleNodes);
+            tempPath.add(nextNode);
+            possibleNodes.remove(nextNode);
+        }
+
+        int[] path = new int[size];
+        for(int a = 0; a < tempPath.size(); a++){
+            path[a] = tempPath.get(a);
+        }
+        return path;
+
+
+    }
+
+    public static int getClosestNeighborForNode(int[][] arcs, int nodeNbr, Set<Integer> validNodes){
+        int neighbourNode = -1;
+        int minDistance = Integer.MAX_VALUE;
+        for(int a = 0; a<arcs[nodeNbr].length;a++){
+           if(validNodes.contains(a) && arcs[nodeNbr][a] != 0 && arcs[nodeNbr][a] < minDistance){
+               neighbourNode = a;
+               minDistance = arcs[nodeNbr][a];
+           }
+        }
+        return neighbourNode;
+    }
+
+    /**
+     * 
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    public static int[] readGraphFromCVSFile(String file) {
+        BufferedReader br = null;
+        String line = "";
+        try {
+            br = new BufferedReader(new FileReader(file));
+            line = br.readLine();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] split = line.split(",");
+        int[] results = new int[split.length];
+
+        for (int i = 0; i < split.length; i++) {
+            try {
+                results[i] = Integer.parseInt(split[i]);
+            } catch (NumberFormatException nfe) {
+                nfe.printStackTrace();
+            }
+        }
+        return results;
+    }
+
+    public static void printPath(int[] path) {
+        System.out.print("Path: ");
+        for (int i = 0; i < path.length; i++) {
+            System.out.print(path[i] + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void printArcs(int size, int[][] distanceArray) {
+        System.out.println(" - Distance array - ");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(" " + distanceArray[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
