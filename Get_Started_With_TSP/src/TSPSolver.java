55b41e34b01415ce2bf3590caac1c045ef11149c
Carl-Emil Kjellstrand <erbsman@gmail.com>
2014-06-19 22:26:42 +0200

    initail get started help package

---
+++ Get_Started_With_TSP/src/TSPSolver.java
@@ -0,0 +1,74 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class TSPSolver {
+
+    private final static int WINDOW_SIZE = 500;
+
+    public static void main(String[] args) {
+
+        final int coordinateSize = 100;
+
+        final int[] data = TSPTools.readGraphFromCVSFile("../nodegen/" + coordinateSize + "_locations.csv");
+
+        int size = data.length / 2;
+
+        int[][] arcs = Arcs.getArray(data, size, size);
+
+        final int[] path = new int[size];
+
+        TSPTools.getRandomizedStartPath(path, System.currentTimeMillis());
+
+            long last = Integer.MAX_VALUE - 1;
+            long best = Integer.MAX_VALUE;
+            while (last < best) {
+                best = last;
+            // TODO improve the path !!!
+
+            }
+
+        TSPTools.checkPath(arcs, path);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI(TSPTools.getPolygonForPlotting(data, path, WINDOW_SIZE, coordinateSize));
+            }
+        });
+
+    }
+
+    private static void createAndShowGUI(Polygon p) {
+        JFrame f = new JFrame("TSP path viewer");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(WINDOW_SIZE, WINDOW_SIZE);
+        f.add(new MyPanel(p));
+        f.pack();
+        f.setVisible(true);
+    }
+
+    static class MyPanel extends JPanel {
+        Polygon p = null;
+
+        public MyPanel(Polygon p) {
+            setBorder(BorderFactory.createLineBorder(Color.black));
+            this.p = p;
+        }
+
+        public Dimension getPreferredSize() {
+            return new Dimension(WINDOW_SIZE, WINDOW_SIZE);
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawPolygon(p);
+        }
+    }
+
+}
